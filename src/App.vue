458c919e4103f29c44cb0f2a7f072f63100be1e2
Coki140 <jg2803638@gmail.com>
2024-10-16 11:55:41 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Indecision />
+</template>
+
+<script setup>
+import Indecision from "./components/Mis componentes/Indecision.vue";
+</script>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  margin: 0 2rem 0 0;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
